e88d11506bb10e84024206d2963eb10e6188d1d5
Sean Phelan <seanphelan97@gmail.com>
2018-08-21 12:12:25 -0600

    added hazardWarningCreator

--- drills.js
+++ drills.js
@@ -44,3 +44,29 @@ console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
 console.log(filter(myNames, (element) => element[0] === 'R'))
+
+function hazardWarningCreator(typeOfWarning) {
+  let warningCounter = 0;
+  return (location) => {
+    warningCounter++;
+    let s = warningCounter === 1 ? '' : 's';
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time${s} today!`);
+  }
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const floodWarning = hazardWarningCreator('Risk of flash floods');
+const ogreWarning = hazardWarningCreator('Ogre');
+
+rocksWarning('Main St and Pacific Ave');
+// => DANGER There is a Rocks on the Road hazard at Main St and Pacific Ave
+// => The Rocks on the Road hazard has triggered 1 time(s) today!
+
+floodWarning('Manitou');
+floodWarning('Atlantas');
+
+rocksWarning('Centinela Ave and Olympic Blvd');
+// => DANGER There is a Rocks on the Road hazard at Centinela Ave and Olympic Blvd
+// => The Rocks on the Road hazard has triggered 2 time(s) today!
+ogreWarning("the swamp");
